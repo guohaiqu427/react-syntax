3dd175be469c13c230bf28732bcabd06b37b9cf3
Guohai Qu <vincent@Guohais-MacBook-Pro.local>
2023-02-24 15:30:38 +0100

    change apicall url

--- src/customHooks/useBreedList.js
+++ src/customHooks/useBreedList.js
@@ -21,7 +21,7 @@ export default function useBreedList(animal) {
     setStatus("loading");
 
     const result = await fetch(
-      `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
     );
     const json = await result.json();
 
