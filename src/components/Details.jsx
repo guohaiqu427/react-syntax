273b34a727640407fd938051858359b4a27558d9
Guohai Qu <vincent@Guohais-MacBook-Pro.local>
2023-02-25 13:25:09 +0100

    class compoonent- carousel

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "../fetch/fetchPet";
+import Carousel from "./Carousel";
 
 export default function Details() {
   const { id } = useParams();
@@ -9,7 +10,13 @@ export default function Details() {
   let pet = {};
   if (!result.isLoading) {
     pet = result.data.pets[0]; // rewrite here!
+    console.log(pet);
   }
 
-  return <div>{pet.name}</div>;
+  return (
+    <div className="details">
+      <Carousel images={pet.images} />
+      {pet.name}
+    </div>
+  );
 }
